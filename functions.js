629eae3734443e5d6a3ea79798b6297467858921
BryOliver <bryan.oliver999@gmail.com>
2020-09-24 11:55:17 -0300

    02 - melhorando as função de acender e apagar

--- functions.js
+++ functions.js
@@ -1,59 +1,9 @@
-function acender1(){
-    var img = document.getElementById("luz1");
+function acender(id){
+    var img = document.getElementById(id);
     img["src"] = "Lon.png"
 }
 
-function apagar1(){
-    var img = document.getElementById("luz1");
-    img["src"] = "Loff.png"
-}
-
-function acender2(){
-    var img = document.getElementById("luz2");
-    img["src"] = "Lon.png"
-}
-
-function apagar2(){
-    var img = document.getElementById("luz2");
-    img["src"] = "Loff.png"
-}
-
-function acender3(){
-    var img = document.getElementById("luz3");
-    img["src"] = "Lon.png"
-}
-
-function apagar3(){
-    var img = document.getElementById("luz3");
-    img["src"] = "Loff.png"
-}
-
-function acender4(){
-    var img = document.getElementById("luz4");
-    img["src"] = "Lon.png"
-}
-
-function apagar4(){
-    var img = document.getElementById("luz4");
-    img["src"] = "Loff.png"
-}
-
-function acender5(){
-    var img = document.getElementById("luz5");
-    img["src"] = "Lon.png"
-}
-
-function apagar5(){
-    var img = document.getElementById("luz5");
-    img["src"] = "Loff.png"
-}
-
-function acender6(){
-    var img = document.getElementById("luz6");
-    img["src"] = "Lon.png"
-}
-
-function apagar6(){
-    var img = document.getElementById("luz6");
+function apagar(id){
+    var img = document.getElementById(id);
     img["src"] = "Loff.png"
 }
