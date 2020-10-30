58cb79a558da8cb568795372de27d4767d966a77
BryOliver <bryan.oliver999@gmail.com>
2020-10-29 23:09:05 -0300

    03 - criando conexão AJAX com o ThingSpeak

--- functions.js
+++ functions.js
@@ -1,9 +1,23 @@
 function acender(id){
     var img = document.getElementById(id);
-    img["src"] = "Lon.png"
+    img["src"] = "img/Lon.png"
+    sendToServer(1000)
 }
 
 function apagar(id){
     var img = document.getElementById(id);
-    img["src"] = "Loff.png"
+    img["src"] = "img/Loff.png"
+    sendToServer(200)
 }
+
+function sendToServer(state){
+    var KEY = "AJGT3RJLIHIQP8LL&field1";
+    //criar um objeto capaz de enviar dados via requisição HTTP GET
+    const http = new XMLHttpRequest();
+    //prepara um GET passando a váriavel lux como ultimo paramentro do link
+    http.open("GET", " https://api.thingspeak.com/update?api_key="+ KEY +"=0"+state);
+    //envia um GET
+    http.send();
+    //quando a requisição retornar ele chama o console e imprime o valor gerado
+    http.onload = console.log(http.responseText+" "+state)   
+} 
